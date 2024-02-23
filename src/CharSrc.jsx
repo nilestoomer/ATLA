9619556ce54e5bb3151ce8e90b2c52705d76fc06
nilestoomer <user@email.com>
2024-02-23 11:30:22 -0500

    fixed char search

--- src/CharSrc.jsx
+++ src/CharSrc.jsx
@@ -5,15 +5,19 @@ import { useState, useEffect} from 'react';
 function CharSrc() {
   const [search, setSearch] = useState("")
   const [char, setChar] = useState({})
-  const fetchChar = async () => {
-    const res = await axios.get(`https://last-airbender-api.fly.dev/api/v1/characters?name=${search}`);
-    console.log(res.data)
-    setChar(res.data[0])
 
-    // useEffect(() => {
-    //   fetchChar();
-    // }, []);
-}
+  const fetchChar = async () => {
+    if (search) {
+      const res = await axios.get(`https://last-airbender-api.fly.dev/api/v1/characters?name=${search}`);
+      console.log(res.data)
+      setChar(res.data[0])
+    }
+  }
+
+  // useEffect(() => {
+  //   fetchChar();
+  // }, [fetchChar]);
+
   return (
     <div>
       <label for="char-search">Search for an ATLA Character</label>
@@ -29,8 +33,14 @@ function CharSrc() {
       type='search'
       >Search</button>
 
-      <img className='char-src-pic' src={char.photoUrl} alt={`Picture of ${char.name}`}/>
+      {
+        char.name && (
+          <>
+        <img className='char-src-pic' src={char.photoUrl} alt={`Picture of ${char.name}`}/>
       <p>Name: {char.name || "wrong"}</p>
+       </>
+        )
+      }
     </div>
   )
 }
